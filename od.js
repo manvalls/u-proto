7fee9ed217c33860795ced9d730a9606e2aa70eb
Manuel Valls Fernández <manolo@vlrz.es>
2015-03-09 17:46:57 +0100

    'on' and 'once' now support an arbitrary number of arguments

--- od.js
+++ od.js
@@ -9,28 +9,49 @@ exports.on = Su();
 exports.once = Su();
 exports.detach = Su();
 
-Su.define(Object.prototype,exports.on,walk.wrap(function*(event,listener,extra){
-  var e;
+Su.define(Object.prototype,exports.on,walk.wrap(function*(){
+  var event = arguments[0],
+      listener = arguments[1];
   
   listener[active] = listener[active] || {};
+  
+  if(listener[active][event] != null){
+    listener[active][event] = true;
+    return;
+  }
+  
   listener[active][event] = true;
   
-  e = yield this[until](event);
+  arguments[1] = event;
+  arguments[0] = yield this[until](event);
+  
   while(listener[active][event]){
-    walk(listener,[e,extra],this);
-    e = yield this[until](event);
+    walk(listener,arguments,this);
+    arguments[0] = yield this[until](event);
   }
+  
+  delete listener[active][event];
   
 }));
 
-Su.define(Object.prototype,exports.once,walk.wrap(function*(event,listener,extra){
-  var e;
+Su.define(Object.prototype,exports.once,walk.wrap(function*(){
+  var event = arguments[0],
+      listener = arguments[1];
   
   listener[active] = listener[active] || {};
+  
+  if(listener[active][event] != null){
+    listener[active][event] = true;
+    return;
+  }
+  
   listener[active][event] = true;
   
-  e = yield this[until](event);
-  if(listener[active][event]) walk(listener,[e,extra],this);
+  arguments[1] = event;
+  arguments[0] = yield this[until](event);
+  
+  if(listener[active][event]) walk(listener,arguments,this);
+  delete listener[active][event];
   
 }));
 
