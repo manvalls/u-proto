f32dec63bddb9782e36b13631518c569dbda8080
Manuel Valls Fernández <manolo@vlrz.es>
2015-06-10 22:02:18 +0200

    Fixed wrong path

--- mirror.js
+++ mirror.js
@@ -1,13 +1,15 @@
 var elem = require('u-elem'),
     Su = require('u-su'),
     
-    connect = require('../connect.js'),
+    connect = require('./connect.js'),
     mirror;
 
 module.exports = mirror = Su();
 
 Su.define(Object.prototype,mirror,function(arr,prop,f,that){
   var obj;
+  
+  console.log(arr);
   
   if(!(arr instanceof Array)){
     
@@ -18,6 +20,8 @@ Su.define(Object.prototype,mirror,function(arr,prop,f,that){
     prop = arr;
     
   }else obj = elem(arr);
+  
+  console.log(arr);
   
   obj.connection = this[connect](obj,prop,f,that);
   
