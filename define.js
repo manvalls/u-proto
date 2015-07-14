757d59b0715ff15039b318986381e81c31603c24
Manuel Valls Fernández <manolo@vlrz.es>
2015-07-14 22:10:41 +0200

    define: now definition of a single property is possible

--- define.js
+++ define.js
@@ -4,6 +4,21 @@ var Su = require('u-su'),
 Su.define(Object.prototype,define,function(obj,desc){
   var keys,i,j,bag;
   
+  if(typeof obj == 'string'){
+    bag = {};
+    bag[obj] = {};
+    
+    bag[obj].value = desc;
+    desc = arguments[2] || {};
+    
+    bag[obj].enumerable = desc.enumerable || false;
+    bag[obj].configurable = desc.configurable || false;
+    bag[obj].writable = desc.writable || false;
+    
+    Object.defineProperties(this,bag);
+    return bag;
+  }
+  
   bag = {};
   desc = desc || {};
   keys = Object.keys(obj);
@@ -14,6 +29,7 @@ Su.define(Object.prototype,define,function(obj,desc){
     
     d.enumerable = desc.enumerable || false;
     d.configurable = desc.configurable || false;
+    if('writable' in d) d.writable = desc.writable || false;
     
     bag[i] = d;
   }
