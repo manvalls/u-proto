9b5fa9b4c4101e62e03bb2702217b3875bfb3046
Manuel Valls Fernández <manolo@vlrz.es>
2015-07-19 22:41:33 +0200

    Now 'define' can handle single symbol definitions

--- define.js
+++ define.js
@@ -3,38 +3,37 @@ var Su = require('u-su'),
 
 Su.define(Object.prototype,define,function(obj,desc){
   var keys,i,j,bag;
-  
-  if(typeof obj == 'string'){
+
+  if(typeof obj != 'object'){
     bag = {};
     bag[obj] = {};
-    
+
     bag[obj].value = desc;
     desc = arguments[2] || {};
-    
+
     bag[obj].enumerable = desc.enumerable || false;
     bag[obj].configurable = desc.configurable || false;
     bag[obj].writable = desc.writable || false;
-    
+
     Object.defineProperties(this,bag);
     return bag;
   }
-  
+
   bag = {};
   desc = desc || {};
   keys = Object.keys(obj);
-  
+
   for(j = 0;j < keys.length;j++){
     i = keys[j];
     d = Object.getOwnPropertyDescriptor(obj,i);
-    
+
     d.enumerable = desc.enumerable || false;
     d.configurable = desc.configurable || false;
     if('writable' in d) d.writable = desc.writable || false;
-    
+
     bag[i] = d;
   }
-  
+
   Object.defineProperties(this,bag);
   return bag;
 });
-
